7880bdca844418589f11706457daad3be71659fb
PetterMartin <petter.aa@icloud.com>
2023-08-31 13:22:58 +0200

    nav-bar-finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/Login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
 
       <main>
         <Outlet />
