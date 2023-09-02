4f620c3594f811c838224e475a0ad02d862305b9
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2023-09-02 02:39:41 +0200

    change of style test

--- src/components/login-form/index.jsx
+++ src/components/login-form/index.jsx
@@ -1,8 +1,8 @@
 import { login } from "../../lib/my-api";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "@tanstack/react-router";
-import logo from "../../assets/Y_logo_PNG (2).png";
+import logo from "../../assets/Y_logo.png";
 import SignUpForm from "../sign-up_form";
 
 // Reusable Input and Button Components
@@ -14,12 +14,12 @@ const Input = ({ type, placeholder, value, onChange, required, minLength }) => (
         onChange={onChange}
         required={true}
         minLength={4}
-        className="bg-neutral-100 border-2 border-[#1E1E1E] text-gray-900 sm:text-sm rounded-3xl focus:ring-sky-100 focus:border-sky-100 block w-full p-2.5"
+        className="bg-neutral-100 border-2 border-[#F5F5F5] text-gray-900 sm:text-sm rounded-3xl focus:ring-sky-100 focus:border-sky-100 block w-full p-2.5"
         aria-label={placeholder}
     />
 );
 const CustomButton = ({ label, onClick }) => (
-    <button onClick={onClick} className="bg-neutral-100 text-gray-900 px-4 py-2 rounded-3xl w-full my-2 border-2 border-[#1E1E1E] shadow-custom">
+    <button onClick={onClick} className="bg-neutral-100 text-gray-900 px-4 py-2 rounded-3xl w-full my-2 border-2 border-[#F5F5F5] hover:border-white shadow-custom">
         {label}
     </button>
 );
@@ -27,22 +27,22 @@ const CustomButton = ({ label, onClick }) => (
 // Loading, Error, and Success Messages
 const Loading = () => (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-        <div className="text-white bg-transparent">
+        <div className="text-white text-xl font-bold bg-transparent">
             Verifying...
         </div>
     </div>
 );
 const ErrorMessage = ({ message }) => (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-        <div className="text-white bg-transparent">
+        <div className="text-white text-xl font-bold bg-transparent">
             An error occurred: {message}
         </div>
     </div>
 );
 const SuccessMessage = () => (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-        <div className="text-white bg-transparent">
-            Login Successful
+        <div className="text-white text-xl font-bold bg-transparent">
+            Login Successful...
         </div>
     </div>
 );
@@ -54,11 +54,16 @@ function LoginForm() {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [spin, setSpin] = useState(false);
 
     const loginMutation = useMutation(async ({ username, password }) => {
         const data = await login({ username, password });
         navigate({ to: "/", params: { id: data.id } });
     });
+
+    useEffect(() => {
+        setSpin(loginMutation.isLoading);
+    }, [loginMutation.isLoading]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -68,30 +73,30 @@ function LoginForm() {
     return (
         <div className="red-100 dark:bg-gray-900 p-4 md:p-10 rounded-lg">
             <div className="flex justify-center items-center">
-                <img className="h-22 sm:h-24 md:h-26 lg:h-28 xl:h-30 my-5" src={logo}></img>
+                <img className={`h-22 sm:h-24 md:h-26 lg:h-28 xl:h-30 my-5 logo ${spin ? 'spin' : ''}`} src={logo}></img>
             </div>
-            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mt-2 mb-5">Better than X!</h1>
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mt-2 mb-5"><p className="text-sm text-neutral-100 dark:text-gray-700">is</p>Better than X!</h1>
             {loginMutation.isLoading ? <Loading /> : null}
             {loginMutation.isError ? <ErrorMessage message={loginMutation.error.message} /> : null}
             {loginMutation.isSuccess ? <SuccessMessage /> : null}
 
             {activeForm === 'login' ? (!showLogin ? (
                 <>
-                    <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold my-2">Sign up now</h2>
-                    <CustomButton label="Sign up with Google" onClick={() => loginMutation.mutate({ username: "kminchelle", password: "0lelplR" })} />
-                    <CustomButton label="Sign up with Apple" onClick={() => loginMutation.mutate({ username: "kminchelle", password: "0lelplR" })} />
-                    <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl  text-center font-bold">or</p>
-                    <button onClick={() => setActiveForm('signup')} className="bg-orange-200 text-gray-900 px-4 py-2 rounded-3xl w-full mt-2 mb-5 border-2 border-[#1E1E1E] shadow-custom">Create an Account</button>
-                    <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold my-2">Already have an account?</h3>
-                    <button onClick={() => setShowLogin(true)} className="orange-200 text-gray-900 px-4 py-2 rounded-3xl w-full my-2 border-2 border-[#1E1E1E] shadow-custom">
-                        Sign In
-                    </button>
+                    <div className="w-full dark:rounded-3xl dark:border-2 md:mt-0 sm:max-w-md p-4 dark:bg-gray-800 dark:border-gray-700">
+                        <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-3xl font-bold my-2">Sign up now</h2>
+                        <CustomButton label="Sign up with Google" onClick={() => loginMutation.mutate({ username: "kminchelle", password: "0lelplR" })} />
+                        <CustomButton label="Sign up with Apple" onClick={() => loginMutation.mutate({ username: "kminchelle", password: "0lelplR" })} />
+                        <p className="text-center font-bold">or</p>
+                        <button onClick={() => setActiveForm('signup')} className="bg-orange-200 text-gray-900 px-4 py-2 rounded-3xl w-full mt-2 mb-5 border-2 border-orange-200 hover:border-orange-100 shadow-custom">Create an Account</button>
+                        <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold my-2">Already have an account?</h3>
+                        <CustomButton label="Sign In" onClick={() => setShowLogin(true)} />
+                    </div>
                 </>
             ) : (
                 <section>
-                    <div className="w-full bg-orange-200 rounded-3xl sm:w-full md:w-1/2shadow-custom border-2 border-[#1E1E1E] md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                        <div className="p-4 space-y-2 sm:space-y-4 md:space-y-6 sm:p-8">
-                            <h2 className="text-l sm:text-xl md:text-2xl lg:text-3xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
+                    <div className="w-full bg-orange-200 rounded-3xl border-2 border-orange-100 mt-1 md:mt-2 sm:max-w-md xl:p-1 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 sm:space-y-5 md:space-y-7 sm:p-8">
+                            <h2 className="text-l sm:text-xl md:text-2xl lg:text-2xl font-bold leading-tight tracking-tight text-gray-800 dark:text-white">
                                 Sign in to your account
                             </h2>
                             <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" action="/profile">
@@ -100,16 +105,16 @@ function LoginForm() {
                                 <div className="flex items-center justify-between">
                                     <div className="flex items-start">
                                         <div className="flex items-center h-5">
-                                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
+                                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 ml-2 border border-gray-100 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
                                         </div>
-                                        <div className="ml-3 text-sm">
-                                            <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                        <div className="ml-1 text-sm">
+                                            <label htmlFor="remember" className="text-gray-600 dark:text-gray-300 text-xs sm:text-xs md:text-xs lg:text-sm font-light">Remember me</label>
                                         </div>
                                     </div>
-                                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                                    <a href="#" className="text-gray-700 dark:text-gray-400 text-xs sm:text-sm md:text-sm lg:text-sm font-medium text-primary-600 hover:underline ml-1">Forgot password?</a>
                                 </div>
-                                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-3xl w-full my-2 border-2 border-[#1E1E1E] shadow-custom">Login</button>
-                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-3xl w-full my-2 border-2 border-blue-500 hover:border-blue-400 shadow-custom">Login</button>
+                                <p className="text-xs sm:text-sm font-light text-gray-700 dark:text-gray-400">
                                     Don’t have an account yet? <a href="#" onClick={() => setShowLogin(false)} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                                 </p>
                             </form>
