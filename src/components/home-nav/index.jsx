83258eefd95a33a6c71dcc0ab5fe82e54b2607b6
PetterMartin <petter.aa@icloud.com>
2023-09-02 17:38:20 +0200

    mobile-home missing nav bar

--- src/components/home-nav/index.jsx
+++ src/components/home-nav/index.jsx
@@ -9,25 +9,25 @@ import CogIcon from "../../assets/icons/cog.svg";
 
 function NavBar() {
   return (
-    <nav className="hidden md:flex bg-white rounded-3xl p-5 w-64 border-2 border-black shadow-custom flex flex-col text-xl">
+    <nav className="hidden md:block bg-white rounded-3xl p-5 w-64 border-2 border-black shadow-custom flex flex-col text-xl">
       <ul className="space-y-6">
         <li className="flex items-center bg-orange-200 dark:bg-blue-500 dark:text-white dark:border-blue-500 dark:hover:border-blue-400  text-gray-900 p-2 w-full mt-auto rounded-3xl border-2 border-orange-200 hover:border-orange-100 shadow-custom leading-tight tracking-tigh">
           <img src={HomeIcon} alt="Home Icon" className="w-6 h-6 mr-2" />
-          <Link to="/" style={{ color: "black" }}>
+          <Link to="/" style={{ color: "black" }} className="hidden md:inline-block">
             Home
           </Link>
         </li>
         <li className="flex items-center p-2">
           <img src={UserIcon} alt="User Icon" className="w-6 h-6 mr-2" />
-          <Link to="/profile" style={{ color: "black" }}>
+          <Link to="/profile" style={{ color: "black" }} className="hidden md:inline-block">
             Profile
           </Link>
         </li>
         <li className="flex items-center p-2">
           <img src={MailIcon} alt="Mail Icon" className="w-6 h-6 mr-2" />
-          <a href="#" style={{ color: "black" }}>
-            Messages
-          </a>
+          <a href="#" style={{ color: "black" }} className="hidden md:inline-block">
+  Messages
+</a>
         </li>
         <li className="flex items-center p-2">
           <img
@@ -35,9 +35,9 @@ function NavBar() {
             alt="Bookmark Icon"
             className="w-6 h-6 mr-2"
           />
-          <a href="#" style={{ color: "black" }}>
-            Bookmarks
-          </a>
+          <a href="#" style={{ color: "black" }} className="hidden md:inline-block">
+  Bookmarks
+</a>
         </li>
         <li className="flex items-center p-2">
           <img
@@ -45,15 +45,15 @@ function NavBar() {
             alt="Community Icon"
             className="w-6 h-6 mr-2"
           />
-          <a href="#" style={{ color: "black" }}>
-            Communities
-          </a>
+          <a href="#" style={{ color: "black" }} className="hidden md:inline-block">
+  Communities
+</a>
         </li>
         <li className="flex items-center p-2">
           <img src={CogIcon} alt="Settings Icon" className="w-6 h-6 mr-2" />
-          <a href="#" style={{ color: "black" }}>
-            Settings
-          </a>
+          <a href="#" style={{ color: "black" }} className="hidden md:inline-block">
+  Settings
+</a>
         </li>
       </ul>
 
