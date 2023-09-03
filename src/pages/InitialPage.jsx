b81a315ec09e9fe04bb0cd10b6fe63244a65d237
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2023-09-03 16:26:36 +0200

    troubleshooting

--- src/pages/InitialPage.jsx
+++ src/pages/InitialPage.jsx
@@ -3,25 +3,43 @@ import HomePage from './Home';
 import LoginPage from './Login';
 import { useQuery } from '@tanstack/react-query';
 import { fetchUser } from '../lib/my-api';
+import useMetaTags from "../hooks/useMetaTags";
+
+
 
 const InitialPage = () => {
+    useMetaTags();
     const [isLoading, setIsLoading] = useState(true);
-    const userId = null;
+    const [userId, setUserId] = useState(4); // Initialize as null
 
-    const { data: user, isLoading: userLoading, isError } = useQuery(['user', userId], () => fetchUser(userId), {
-        enabled: !!userId,
-        onSuccess: () => setIsLoading(false),
-    });
+
+    useEffect(() => {
+        // Simulate a 3-second loading time before showing LoginPage
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+    }, []);
+
+    // Fetch User
+    const { data: user, isLoading: userLoading, isError } = useQuery(
+        ['user', userId],
+        () => fetchUser(userId),
+        {
+            enabled: !!userId,
+            onSuccess: () => setIsLoading(false),
+            onError: () => setIsLoading(false)
+        }
+    );
 
     if (isLoading || userLoading) {
         return <span>Loading...</span>;
     }
 
-    if (isError) {
+    if (isError || !userId) {
         return <LoginPage />;
     }
 
-    return user ? <HomePage /> : <LoginPage />;
+    return <HomePage />;
 };
 
 export default InitialPage;
