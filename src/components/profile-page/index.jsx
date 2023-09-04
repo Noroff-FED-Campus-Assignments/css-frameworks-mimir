48356f45a3b3e9bc8f0999f433b525bee1090818
Khadarrr <111360349+Khadarrr@users.noreply.github.com>
2023-09-04 16:48:44 +0200

    text-box for profile page

--- src/components/profile-page/index.jsx
+++ src/components/profile-page/index.jsx
@@ -50,17 +50,50 @@ function ProfilePage() {
           <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-3xl font-semibold text-gray-800 dark:text-white">
             {userData.firstName} {userData.lastName}
           </h1>
-          <p className="text-gray-600 dark:text-white">Eye color: {userData.eyeColor}</p>
+          <p className="text-gray-600 dark:text-white">
+            Eye color: {userData.eyeColor}
+          </p>
         </div>
         {/* Bio */}
         <div className="px-6 py-4">
-          <h2 className="text-base sm:text-lg md:text-xl font-semibold text-gray-800 dark:text-white">Bio</h2>
-          <p className="text-sm sm:text-sm md:text-sm text-gray-600 dark:text-white">{userData.bio}I DANCE FOR MONEY</p>
+          <h2 className="text-base sm:text-lg md:text-xl font-semibold text-gray-800 dark:text-white">
+            Bio
+          </h2>
+          <p className="text-sm sm:text-sm md:text-sm text-gray-600 dark:text-white">
+            {userData.bio}I DANCE FOR MONEY
+          </p>
+        </div>
+      </div>
+      <div className="bg-white w-full mt-2 text-xl p-4 mb-8 shadow-custom rounded-3xl border-2 border-black">
+        <div className="flex items-start space-x-4">
+          <img className="w-10 h-10 rounded-full" />
+
+          <div className="flex-grow">
+            <textarea
+              placeholder="What's happening?"
+              className="w-full h-20 resize-none border border-gray-300 p-2 rounded-md focus:outline-none"
+            ></textarea>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between mt-2 ms-14">
+          <button
+            className="bg-neutral-100 text-gray-900 leading-tight tracking-tight px-3 py-2 rounded-lg w-24 my-2 border-2 border-[#F5F5F5] hover:border-white dark:hover:bg-gray-100 dark:bg-gray-200 dark:border-gray-200 shadow-custom"
+            style={{
+              backgroundColor: accentColor,
+              color: "white",
+              fontSize: "14px",
+            }}
+          >
+            Post
+          </button>
         </div>
       </div>
       {/* Posts Section */}
       <div className="mt-1 w-full bg-orange-200 p-6 rounded-3xl border-2 border-orange-100 dark:bg-gray-800 dark:border-gray-700">
-        <h1 className="text-xl sm:text-2xl font-bold text-gray-800 dark:text-white mb-4 ml-2">POSTS</h1>
+        <h1 className="text-xl sm:text-2xl font-bold text-gray-800 dark:text-white mb-4 ml-2">
+          POSTS
+        </h1>
         {postsData.posts.map((post) => (
           <div
             key={post.id}
@@ -68,7 +101,9 @@ function ProfilePage() {
           >
             {/* Post Content */}
             <div className="mt-2">
-              <p className="text-sm text-gray-600 dark:text-white mb-2">{post.body}</p>
+              <p className="text-sm text-gray-600 dark:text-white mb-2">
+                {post.body}
+              </p>
             </div>
 
             {/* Divider Line */}
@@ -86,7 +121,9 @@ function ProfilePage() {
                   <h3 className="text-sm text-gray-600 dark:text-white hover:scale-105 hidden xxs:inline-block">
                     {userData.firstName} {userData.lastName}
                   </h3>
-                  <p className="text-sm text-black dark:text-white">@{userData.username}</p>
+                  <p className="text-sm text-black dark:text-white">
+                    @{userData.username}
+                  </p>
                 </div>
               </div>
               {/* Like and Comment Buttons */}
